852efa67defe782d91e5fc6cf775896cde5e84fe
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-04-10 18:32:47 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -4,27 +4,29 @@ bod3 = 0
 bod4 = 0
 bod5 = 0
 bod6 = 0
-
+A = 0
 
 led.plot_brightness(bod1 + 2, bod2 + 2, 100)
 
 
 def on_button_pressed_a():
-    global bod1, bod2, bod3, bod4, bod5, bod6
-    bod1 += 4
-    bod2 += 2
-    led.plot(bod1, bod2)
+    global bod1, bod2, bod3, bod4, bod5, bod6, A
+    if A == 0:
+        bod1 += 4
+        bod2 += 2
+        led.plot(bod1, bod2)
 
-    bod3 += 0
-    bod4 += 0
-    led.plot(bod3, bod4)
+        bod3 += 0
+        bod4 += 0
+        led.plot(bod3, bod4)
 
-    bod5 += 0
-    bod6 += 4
-    led.plot(bod5, bod6)
-    led.plot_brightness(2, 2, 100)
+        bod5 += 0
+        bod6 += 4
+        led.plot(bod5, bod6)
+        led.plot_brightness(2, 2, 100)
+        A += 1
 
-    if bod1 == 4 and bod2 == 2 and bod3 == 0 and bod4 == 0 and bod5 == 0 and bod6 == 4:
+    if A == 1:
         basic.clear_screen()
         bod1 -= 2
         bod2 += 2
--- main.ts
+++ main.ts
@@ -4,20 +4,25 @@ let bod3 = 0
 let bod4 = 0
 let bod5 = 0
 let bod6 = 0
+let A = 0
 led.plotBrightness(bod1 + 2, bod2 + 2, 100)
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    bod1 += 4
-    bod2 += 2
-    led.plot(bod1, bod2)
-    bod3 += 0
-    bod4 += 0
-    led.plot(bod3, bod4)
-    bod5 += 0
-    bod6 += 4
-    led.plot(bod5, bod6)
-    led.plotBrightness(2, 2, 100)
-    if (bod1 == 4 && bod2 == 2 && bod3 == 0 && bod4 == 0 && bod5 == 0 && bod6 == 4) {
+    if (A == 0) {
+        bod1 += 4
+        bod2 += 2
+        led.plot(bod1, bod2)
+        bod3 += 0
+        bod4 += 0
+        led.plot(bod3, bod4)
+        bod5 += 0
+        bod6 += 4
+        led.plot(bod5, bod6)
+        led.plotBrightness(2, 2, 100)
+        A += 1
+    }
+    
+    if (A == 1) {
         basic.clearScreen()
         bod1 -= 2
         bod2 += 2
