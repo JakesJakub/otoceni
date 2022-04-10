7ad07a01e722e15752eeb02b72794072c7696f30
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-04-10 19:51:37 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 A = 0
-
+B = 0
 led.plot(2, 0)
 led.plot(0, 4)
 led.plot(4, 4)
@@ -37,3 +37,36 @@ def on_button_pressed_a():
         led.plot_brightness(2, 2, 100)
         A -= 4
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global B
+    basic.clear_screen()
+    B += 1
+    if B == 1:
+        led.plot(2, 0)
+        led.plot(0, 4)
+        led.plot(4, 4)
+        led.plot_brightness(2, 2, 100)
+
+    if B == 2:
+        basic.clear_screen()
+        led.plot(0, 2)
+        led.plot(4, 0)
+        led.plot(4, 4)
+        led.plot_brightness(2, 2, 100)
+
+    if B == 3:
+        basic.clear_screen()
+        led.plot(2, 4)
+        led.plot(4, 0)
+        led.plot(0, 0)
+        led.plot_brightness(2, 2, 100)
+
+    if B == 4:
+        basic.clear_screen()
+        led.plot(4, 2)
+        led.plot(0, 0)
+        led.plot(0, 4)
+        led.plot_brightness(2, 2, 100)
+        B -= 4
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let A = 0
+let B = 0
 led.plot(2, 0)
 led.plot(0, 4)
 led.plot(4, 4)
@@ -37,6 +38,43 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
         led.plot(4, 4)
         led.plotBrightness(2, 2, 100)
         A -= 4
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    basic.clearScreen()
+    B += 1
+    if (B == 1) {
+        led.plot(2, 0)
+        led.plot(0, 4)
+        led.plot(4, 4)
+        led.plotBrightness(2, 2, 100)
+    }
+    
+    if (B == 2) {
+        basic.clearScreen()
+        led.plot(0, 2)
+        led.plot(4, 0)
+        led.plot(4, 4)
+        led.plotBrightness(2, 2, 100)
+    }
+    
+    if (B == 3) {
+        basic.clearScreen()
+        led.plot(2, 4)
+        led.plot(4, 0)
+        led.plot(0, 0)
+        led.plotBrightness(2, 2, 100)
+    }
+    
+    if (B == 4) {
+        basic.clearScreen()
+        led.plot(4, 2)
+        led.plot(0, 0)
+        led.plot(0, 4)
+        led.plotBrightness(2, 2, 100)
+        B -= 4
     }
     
 })
