c1da68a9e18a7042a0605cf63ad45c37f0c3044c
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-04-10 19:46:12 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-bod1 = 0
-bod2 = 0
-bod3 = 0
-bod4 = 0
-bod5 = 0
-bod6 = 0
 A = 0
 
-led.plot_brightness(bod1 + 2, bod2 + 2, 100)
-
+led.plot(2, 0)
+led.plot(0, 4)
+led.plot(4, 4)
+led.plot_brightness(2, 2, 100)
 
 def on_button_pressed_a():
-    global bod1, bod2, bod3, bod4, bod5, bod6, A
-    if A == 0:
-        bod1 += 4
-        bod2 += 2
-        led.plot(bod1, bod2)
-
-        bod3 += 0
-        bod4 += 0
-        led.plot(bod3, bod4)
-
-        bod5 += 0
-        bod6 += 4
-        led.plot(bod5, bod6)
-        led.plot_brightness(2, 2, 100)
-        A += 1
-
+    global A
+    basic.clear_screen()
+    A += 1
     if A == 1:
-        basic.clear_screen()
-        bod1 -= 2
-        bod2 += 2
-        led.plot(bod1, bod2)
-
-        bod3 += 4
-        bod4 += 0
-        led.plot(bod3, bod4)
-
-        bod5 += 0
-        bod6 -= 4
-        led.plot(bod5, bod6)
+        led.plot(4, 2)
+        led.plot(0, 0)
+        led.plot(0, 4)
         led.plot_brightness(2, 2, 100)
 
+    if A == 2:
+        basic.clear_screen()
+        led.plot(2, 4)
+        led.plot(4, 0)
+        led.plot(0, 0)
+        led.plot_brightness(2, 2, 100)
+
+    if A == 3:
+        basic.clear_screen()
+        led.plot(0, 2)
+        led.plot(4, 0)
+        led.plot(4, 4)
+        led.plot_brightness(2, 2, 100)
+
+    if A == 4:
+        basic.clear_screen()
+        led.plot(2, 0)
+        led.plot(0, 4)
+        led.plot(4, 4)
+        led.plot_brightness(2, 2, 100)
+        A -= 4
 input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,39 +1,42 @@
-let bod1 = 0
-let bod2 = 0
-let bod3 = 0
-let bod4 = 0
-let bod5 = 0
-let bod6 = 0
 let A = 0
-led.plotBrightness(bod1 + 2, bod2 + 2, 100)
+led.plot(2, 0)
+led.plot(0, 4)
+led.plot(4, 4)
+led.plotBrightness(2, 2, 100)
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    if (A == 0) {
-        bod1 += 4
-        bod2 += 2
-        led.plot(bod1, bod2)
-        bod3 += 0
-        bod4 += 0
-        led.plot(bod3, bod4)
-        bod5 += 0
-        bod6 += 4
-        led.plot(bod5, bod6)
+    basic.clearScreen()
+    A += 1
+    if (A == 1) {
+        led.plot(4, 2)
+        led.plot(0, 0)
+        led.plot(0, 4)
         led.plotBrightness(2, 2, 100)
-        A += 1
     }
     
-    if (A == 1) {
+    if (A == 2) {
         basic.clearScreen()
-        bod1 -= 2
-        bod2 += 2
-        led.plot(bod1, bod2)
-        bod3 += 4
-        bod4 += 0
-        led.plot(bod3, bod4)
-        bod5 += 0
-        bod6 -= 4
-        led.plot(bod5, bod6)
+        led.plot(2, 4)
+        led.plot(4, 0)
+        led.plot(0, 0)
         led.plotBrightness(2, 2, 100)
+    }
+    
+    if (A == 3) {
+        basic.clearScreen()
+        led.plot(0, 2)
+        led.plot(4, 0)
+        led.plot(4, 4)
+        led.plotBrightness(2, 2, 100)
+    }
+    
+    if (A == 4) {
+        basic.clearScreen()
+        led.plot(2, 0)
+        led.plot(0, 4)
+        led.plot(4, 4)
+        led.plotBrightness(2, 2, 100)
+        A -= 4
     }
     
 })
